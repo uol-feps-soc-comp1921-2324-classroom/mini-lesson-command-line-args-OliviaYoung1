c836a12b4cc1b6f1f4566cb240d8b5ef68d256f2
Olivia Young <146246572+OliviaYoung1@users.noreply.github.com>
2024-01-29 13:25:19 +0000

    project lab 1

--- tasks/task.c
+++ tasks/task.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-int main() {
-    int num1, num2;
 
-    // Prompt the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+int main(int argc, char *argv[])
+{
 
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+    if (argc != 3)
+    {
+        printf("Enter exactly 2 numbers\n");
+        return 0;
+    }
 
-    // Calculate and print the sum
-    int sum = num1 + num2;
-    printf("Sum: %d\n", sum);
+    for (int a = 1; a < 3; a++)
+    {
+    for (int i = 0; i < strlen(argv[a]); i++)
+    {
+        if (!isdigit(argv[a][i]))
+        {
+            printf("Enter integers only\n");
+            return 0;
+        }
+    }
+    }
 
-    return 0; // Exit successfully
+
+int sum = atoi(argv[2]) + atoi(argv[1]);
+printf("Sum: %d\n", sum);
+
+return 0; // Exit successfully
 }
